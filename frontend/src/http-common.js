55a8f942cddd79ecb6659187fe9f43b145319919
Mohamed Al-Marzuk <mohamed.al-marzuk@usach.cl>
2024-11-03 14:56:23 -0300

    test

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-const payrollBackendServer = import.meta.env.VITE_PAYROLL_BACKEND_SERVER;
-const payrollBackendPort = import.meta.env.VITE_PAYROLL_BACKEND_PORT;
-
-console.log(payrollBackendServer)
-console.log(payrollBackendPort)
-
 export default axios.create({
-    baseURL: `http://${payrollBackendServer}:${payrollBackendPort}`,
+    baseURL: "http://localhost:80",
     headers: {
         'Content-Type': 'application/json'
     }
