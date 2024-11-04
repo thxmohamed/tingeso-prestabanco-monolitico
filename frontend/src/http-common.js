d3b3a385464802ebeb1882cfca8da25054d071c0
Mohamed Al-Marzuk <mohamed.al-marzuk@usach.cl>
2024-11-04 20:49:27 -0300

    Strating with jenkins

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+//const prestaBancoServer = "prestabanco-mohamed.westus2.cloudapp.azure.com";
+const prestaBancoServer = "localhost:80";
+
 export default axios.create({
-    baseURL: "http://localhost:80",
+    baseURL: `http://${prestaBancoServer}`,
     headers: {
         'Content-Type': 'application/json'
-    }
+    } 
 });
