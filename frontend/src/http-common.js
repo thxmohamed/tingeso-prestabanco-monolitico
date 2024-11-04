69c5a8113f719d95bd066e567d5ae0267203a3d9
MOHAMED AL-MARZUK <115362866+thxmohamed@users.noreply.github.com>
2024-11-04 10:49:06 -0300

    Update http-common.js

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8091",
+    baseURL: "http://localhost:80",
     headers: {
         'Content-Type': 'application/json'
     }
